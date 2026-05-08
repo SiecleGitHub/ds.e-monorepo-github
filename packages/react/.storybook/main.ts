b1ec6eab41bc8313c69fd7edf5465d82fdd42fa1
Clemens Sietas <siecle@gmx.de>
2026-05-08 14:25:32 +0200

    fix: use direct relative path for @ds.e/scss resolution
    - Use ../../scss relative path from .storybook directory
    - Avoid package.json export issues with getAbsolutePath

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -26,7 +26,7 @@ const config: StorybookConfig = {
     return mergeConfig(config, {
       resolve: {
         alias: {
-          '@ds.e/scss': getAbsolutePath('@ds.e/scss'),
+          '@ds.e/scss': resolve(dirname(fileURLToPath(import.meta.url)), '../../scss'),
         },
       },
       css: {
